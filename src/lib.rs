254bb85f682327dedca717cb2c605d7c2ba41e6c
code-mcx <1827939333@qq.com>
2021-03-07 15:06:07 +0800

    Delete comment code

--- src/lib.rs
+++ src/lib.rs
@@ -6,16 +6,3 @@ mod macros;
 pub use feignhttp_codegen::*;
 
 pub struct HttpClient {}
-
-//const URL: &str = "http://www.baidu.com";
-
-// #[get(url = URL)]
-// pub async fn http_get(#[param] aa: [i32; 5], bb: i32) -> Result<String, String> {}
-
-//struct Http {}
-
-// #[feign("http://www.baidu.com")]
-//impl Http {
-    // #[get("/11111111")]
-    // pub async fn get() -> Result<String, String> {}
-//}
