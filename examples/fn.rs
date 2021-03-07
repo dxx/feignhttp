bf9342f3ce5d3d43f9563f58026ff586c8e6b273
code-mcx <1827939333@qq.com>
2021-03-07 14:23:28 +0800

    Add fn example

--- examples/fn.rs
+++ examples/fn.rs
@@ -1,30 +1,83 @@
-use feignhttp::{get, post};
+use feignhttp::{get};
 
 #[get("https://api.github.com")]
-async fn get() -> Result<String, Box<dyn std::error::Error>> {}
+async fn github() -> Result<String, Box<dyn std::error::Error>> {}
 
-#[get("https://api.github.com/users/{user}")]
-async fn get_user(#[path] user: &str) -> Result<String, Box<dyn std::error::Error>> {}
+// Specify path parameter
+#[get("https://api.github.com/repos/{owner}/{repo}")]
+async fn repository (
+    #[path] owner: &str,
+    #[path] repo: String,
+) -> Result<String, Box<dyn std::error::Error>> {}
 
-#[get("https://api.github.com/users/{username}/followers")]
-async fn get_followers(
-    #[path("username")] user: String,
+// Specify path parameter name and query parameter
+#[get("https://api.github.com/repos/{owner}/{repo}/contributors")]
+async fn contributors (
+    #[path("owner")] user: &str,
+    #[path] repo: &str,
+    #[param] page: u32,
+) -> Result<String, Box<dyn std::error::Error>> {}
+
+
+// Specify header
+#[get("https://api.github.com/repos/{owner}/{repo}/commits")]
+async fn commits (
+    #[header] accept: &str,
+    #[path] owner: &str,
+    #[path] repo: &str,
     #[param] page: u32,
     #[param] per_page: u32,
 ) -> Result<String, Box<dyn std::error::Error>> {}
 
+
+const GITHUB_URL: &str = "https://api.github.com";
+
+// Using url constants
+#[get(GITHUB_URL, path = "/repos/{owner}/{repo}/languages")]
+async fn languages (
+    #[path] owner: &str,
+    #[path] repo: &str,
+)-> Result<String, Box<dyn std::error::Error>> {}
+
+
 #[tokio::main]
-async fn main() {
-    let r = get().await.unwrap();
-    println!("get result: {}", r);
+async fn main() -> Result<(), Box<dyn std::error::Error>>  {
+    let r = github().await?;
 
-    let r = get_user("code-mcx").await.unwrap();
-    println!("get_user result: {}", r);
+    println!("github result: {}", r);
 
-    let r = get_followers(
-        "code-mcx".to_string(),
-        1, 5
-    ).await.unwrap();
 
-    println!("get_followers result: {}", r);
+    let r = repository("code-mcx", "feignhttp".to_string()).await?;
+
+    println!("repository result: {}", r);
+
+
+    let r = contributors (
+        "code-mcx",
+        "feignhttp",
+        1
+    ).await?;
+
+    println!("contributors result: {}", r);
+
+
+    let r = commits (
+        "application/vnd.github.v3+json",
+        "code-mcx",
+        "feignhttp",
+        1, 5,
+    ).await?;
+
+    println!("commits result: {}", r);
+
+
+    let r = languages (
+        "code-mcx",
+        "feignhttp",
+    ).await?;
+
+    println!("languages result: {}", r);
+
+
+    Ok(())
 }
