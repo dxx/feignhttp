2abb22779c818f9bdd0fbe2833a5a396ec5ee266
dxx <1827939333@qq.com>
2021-04-18 15:51:33 +0800

    Add examples

---
+++ examples/form.rs
@@ -0,0 +1,31 @@
+use feignhttp::post;
+
+use serde::{Serialize};
+
+// Serialize: Specifies serialization
+#[derive(Debug, Serialize)]
+struct User {
+    id: i32,
+    name: String,
+}
+
+#[post(url = "https://httpbin.org/anything")]
+async fn post_user(#[form] id: i32, #[form] name: &str) -> feignhttp::Result<String> {}
+
+#[post(url = "https://httpbin.org/anything")]
+async fn post_user2(#[form] user: User) -> feignhttp::Result<String> {}
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let r = post_user(1, "jack").await?;
+    println!("{}", r);
+
+    let user = User {
+        id: 1,
+        name: "jack".to_string(),
+    };
+    let r = post_user2(user).await?;
+    println!("{}", r);
+
+    Ok(())
+}
