b281b8637f9aafe9d665678b62dd3f660de3c599
dxx <dx1898@foxmail.com>
2022-01-11 04:15:02 +0000

    Modify example

--- examples/timeout.rs
+++ examples/timeout.rs
@@ -1,6 +1,6 @@
 use feignhttp::{get, feign};
 
-#[get(url = "http://xxx.com", connect_timeout = 3000)]
+#[get(url = "http://site_dne.com", connect_timeout = 3000)]
 async fn connect_timeout() -> feignhttp::Result<String> {}
 
 #[get(url = "https://httpbin.org/delay/5", timeout = 3000)]
@@ -8,7 +8,7 @@ async fn timeout() -> feignhttp::Result<String> {}
 
 pub struct Http;
 
-#[feign(url = "http://xxx.com", connect_timeout = 3000)]
+#[feign(url = "http://site_dne.com", connect_timeout = 3000)]
 impl Http {
     #[get("", connect_timeout = 5000)] // 5000 will override 3000
     async fn get() -> feignhttp::Result<String> {}
