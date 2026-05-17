bcb7811e2f0972ccc16fc27508951879ebd2cf67
dxx <dx1898@foxmail.com>
2026-05-17 23:23:05 +0800

    Modify examples

--- examples/timeout.rs
+++ examples/timeout.rs
@@ -4,42 +4,62 @@ use feignhttp::{FeignClientBuilder, feign, get};
 #[get(url = "http://site_dne.com")]
 async fn default_connect_timeout() -> feignhttp::Result<String> {}
 
-/// The default timeout is 10000 milliseconds.
+/// The default timeout is 10000 milliseconds, override the default timeout.
 #[get(url = "https://httpbin.org/delay/5", timeout = 3000)]
 async fn default_timeout() -> feignhttp::Result<String> {}
 
-#[feign(url = "http://site_dne.com")]
-pub trait Http {
-    #[get("")] // The default connect_timeout and timeout are 10000 milliseconds..
+// The default timeout for all requests in the trait is 10000 milliseconds.
+#[feign(url = "https://httpbin.org")]
+pub trait TimeoutClient {
+    // The default timeout is 10000 milliseconds.
+    #[get(path = "/delay/5")]
     async fn default_timeout(&self) -> feignhttp::Result<String>;
+
+    // Override the default timeout.
+    #[get(path = "/delay/5", timeout = 3000)]
+    async fn custom_timeout(&self) -> feignhttp::Result<String>;
 }
 
 #[tokio::main]
 async fn main() {
     match default_connect_timeout().await {
         Ok(res) => {
-            println!("default_connect_timeout: {}", res);
+            println!("default_connect_timeout ok: {}", res);
         }
         Err(err) => {
-            println!("default_connect_timeout: {:?}", err);
+            // Execute here.
+            println!("default_connect_timeout err: {:?}", err);
         }
     }
 
     match default_timeout().await {
         Ok(res) => {
-            println!("default_timeout: {}", res);
+            println!("default_timeout ok: {}", res);
         }
         Err(err) => {
-            println!("default_timeout: {:?}", err);
+            // Execute here.
+            println!("default_timeout err: {:?}", err);
         }
     }
 
-    match Http::builder().build().unwrap().default_timeout().await {
+    let http_client = TimeoutClient::builder().build().unwrap();
+    match http_client.default_timeout().await {
         Ok(res) => {
-            println!("Http::default_timeout: {}", res);
+            // Execute here.
+            println!("TimeoutClient.default_timeout ok: {}", res);
         }
         Err(err) => {
-            println!("Http::default_timeout: {:?}", err);
+            println!("TimeoutClient.default_timeout err: {:?}", err);
+        }
+    }
+
+    match http_client.custom_timeout().await {
+        Ok(res) => {
+            println!("TimeoutClient.custom_timeout ok: {}", res);
+        }
+        Err(err) => {
+            // Execute here.
+            println!("TimeoutClient.custom_timeout err: {:?}", err);
         }
     }
 }
