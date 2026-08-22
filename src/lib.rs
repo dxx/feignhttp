9fa12295bc8a2dcb3461bb4502612f9615627ac1
dxx <dx1898@foxmail.com>
2026-08-22 10:24:11 +0800

    Modify example of response error

--- src/lib.rs
+++ src/lib.rs
@@ -615,9 +615,10 @@
 //!             if err.is_status_error() {
 //!                 println!("status_error: {}", err);
 //!             }
-//!             if let ErrorKind::Status(status) = err.error_kind() {
+//!             if let ErrorKind::Status(status, body) = err.error_kind() {
 //!             
-//!                 println!("status error code: {}", status.as_u16());
+//!                 println!("error status code: {}", status.as_u16());
+//!                 println!("error body: {}", body);
 //!
 //!                 if status.is_client_error() {
 //!                     // Handle error.
